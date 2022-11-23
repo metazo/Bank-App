a449654957e47473365634fa10eca4900a9177f0
Mouahamed Mboup <mouhamed-el-moustapha.mboup@epitech.eu>
2022-11-23 15:16:48 +0100

    itl commit

--- BankApp/src/BankingApplication.java
+++ BankApp/src/BankingApplication.java
@@ -3,5 +3,18 @@ public class BankingApplication {
 	public static void main(String[] args) {
 		System.out.println(2+2);
 	}
+	
+	
 
 }
+
+class BankAccount {
+		int balance;
+		int previousTransaction;
+		String customerName;
+		String customerId;
+		
+		void deposit(int amount) {
+			
+		}
+	}
